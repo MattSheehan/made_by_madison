8b8c3274f22a52f99e9f58d98cf0dcccf14043bd
Matt Sheehan <MattSheehan@users.noreply.github.com>
2024-07-07 19:26:24 -0600

    UPDATE: init (Shop > Store > Department > Category > Filter) structure (except for jewlery because jesus christ there are going to be NUMEROUS 'department' routes and with routes comes components so another day), nav bar, and google auth (front-end). TODO: finish stripes api, knowledge graphs (offline, on-paper)

--- app/src/components/search/SearchBar.component.jsx
+++ app/src/components/search/SearchBar.component.jsx
@@ -1,16 +1,47 @@
 import { Fragment } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import { MdAccountCircle, MdSettings } from "react-icons/md";
+import { useState } from 'react';
+import FormInput from '../forms/form-input/FormInput.component'
 import './SearchBar.styles.scss';
 const SearchBar = () => {
+  const [ formFields, setFormFields ] = useState({ searchValue:'' });
+  const { searchValue } = formFields;
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      setFormFields({ searchValue: '' });
+    } catch (error) { 
+      console.log(error);
+    }
+  };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
   return (
-    <Fragment>
-      <div className='logo-container'>
-        <Link className='logo' to='/settings'><div><h2><MdSettings/></h2></div></Link>
-        <Link className='logo' to='/auth'><div><h2><MdAccountCircle/></h2></div></Link>
-        <Outlet />
+    <div className='container'>
+      <div className='search-container'>
+        <form className='search-form-container' onSubmit={handleSubmit}>
+          <FormInput
+            label='Search'
+            type='text'
+            required={false}
+            autoComplete='on'
+            onChange={handleChange}
+            name='searchValue'
+            value={searchValue}
+          />
+        </form>
       </div>
-    </Fragment>
+      <Fragment>
+        <div className='logo-container'>
+          <Link className='logo' to='/settings'><div><h2><MdSettings/></h2></div></Link>
+          <Link className='logo' to='/auth'><div><h2><MdAccountCircle/></h2></div></Link>
+        </div>
+        <Outlet />
+      </Fragment>
+    </div>
   );
 };
 export default SearchBar;
